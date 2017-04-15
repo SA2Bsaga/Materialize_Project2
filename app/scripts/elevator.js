47eabffb60fcb5f9aa881b0e2698228b82174b19
SA2Bsaga <ramarbro194@gmail.com>
2017-04-15 11:33:45 -0400

    more functionality with elevator!

--- app/scripts/elevator.js
+++ app/scripts/elevator.js
@@ -1,215 +1,529 @@
 /*!
+
  * Elevator.js
+
  *
+
  * MIT licensed
+
  * Copyright (C) 2015 Tim Holman, http://tholman.com
+
  */
 
+
+
 /*********************************************
+
  * Elevator.js
+
  *********************************************/
 
-;(function(win) {
-  'use strict';
 
-  win.Elevator = function(options) {
+
+var Elevator = function(options) {
+
+
+
+    'use strict';
+
+
 
     // Elements
+
     var body = null;
 
+
+
     // Scroll vars
+
     var animation = null;
+
     var duration = null; // ms
+
     var customDuration = false;
+
     var startTime = null;
+
     var startPosition = null;
+
+    var endPosition = 0;
+
+    var targetElement = null;
+
+    var verticalPadding = null;
+
     var elevating = false;
 
+
+
+    var startCallback;
+
     var mainAudio;
+
     var endAudio;
+
+    var endCallback;
+
+
 
     var that = this;
 
+
+
     /**
+
      * Utils
+
      */
+
+
 
     // Thanks Mr Penner - http://robertpenner.com/easing/
-    function easeInOutQuad(t, b, c, d) {
-      t /= d / 2;
 
-      if (t < 1) {
-        return c / 2 * t * t + b;
-      }
+    function easeInOutQuad( t, b, c, d ) {
 
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
+        t /= d / 2;
+
+        if ( t < 1 ) return c / 2 * t * t + b;
+
+        t--;
+
+        return -c / 2 * ( t * ( t -2 ) - 1 ) + b;
+
     }
 
-    function extendParameters(options, defaults) {
-      for (var option in defaults) {
-        var t = options[option] === undefined && typeof option !== 'function';
-        if (t) {
-          options[option] = defaults[option];
+
+
+    function extendParameters(options, defaults){
+
+        for( var option in defaults ){
+
+            var t = options[option] === undefined && typeof option !== "function";
+
+            if(t){
+
+                options[option] = defaults[option];
+
+            }
+
         }
-      }
-      return options;
+
+        return options;
+
     }
+
+
+
+    function getVerticalOffset(element) {
+
+        var verticalOffset = 0;
+
+        while( element ){
+
+            verticalOffset += element.offsetTop || 0;
+
+            element = element.offsetParent;
+
+        }
+
+
+
+        if ( verticalPadding ) {
+
+          verticalOffset = verticalOffset - verticalPadding;
+
+        }
+
+
+
+        return verticalOffset;
+
+    }
+
+
 
     /**
+
      * Main
+
      */
 
+
+
     // Time is passed through requestAnimationFrame, what a world!
-    function animateLoop(time) {
-      if (!startTime) {
-        startTime = time;
-      }
 
-      var timeSoFar = time - startTime;
-      var easedPosition = easeInOutQuad(timeSoFar, startPosition, -startPosition, duration);
+    function animateLoop( time ) {
 
-      window.scrollTo(0, easedPosition);
+        if ( !startTime ) {
 
-      if (timeSoFar < duration) {
-        animation = requestAnimationFrame(animateLoop);
-      } else {
-        animationFinished();
-      }
-     }
+            startTime = time;
 
-  //            ELEVATE!
-  //              /
-  //         ____
-  //       .'    '=====<0
-  //       |======|
-  //       |======|
-  //       [IIIIII[\--()
-  //       |_______|
-  //       C O O O D
-  //      C O  O  O D
-  //     C  O  O  O  D
-  //     C__O__O__O__D
-  //    [_____________]
+        }
+
+
+
+        var timeSoFar = time - startTime;
+
+        var easedPosition = easeInOutQuad(timeSoFar, startPosition, endPosition - startPosition, duration);
+
+
+
+        window.scrollTo(0, easedPosition);
+
+
+
+        if( timeSoFar < duration ) {
+
+            animation = requestAnimationFrame(animateLoop);
+
+        } else {
+
+            animationFinished();
+
+        }
+
+    }
+
+
+
+//            ELEVATE!
+
+//              /
+
+//         ____
+
+//       .'    '=====<0
+
+//       |======|
+
+//       |======|
+
+//       [IIIIII[\--()
+
+//       |_______|
+
+//       C O O O D
+
+//      C O  O  O D
+
+//     C  O  O  O  D
+
+//     C__O__O__O__D
+
+//    [_____________]
+
     this.elevate = function() {
 
-      if (elevating) {
-        return;
-      }
 
-      elevating = true;
-      startPosition = (document.documentElement.scrollTop || body.scrollTop);
 
-      // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
-      if (!customDuration) {
-        duration = (startPosition * 1.5);
-      }
+        if( elevating ) {
 
-      requestAnimationFrame(animateLoop);
+            return;
 
-      // Start music!
-      if (mainAudio) {
-        mainAudio.play();
-      }
+        }
+
+
+
+        elevating = true;
+
+        startPosition = (document.documentElement.scrollTop || body.scrollTop);
+
+        updateEndPosition();
+
+
+
+        // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
+
+        if( !customDuration ) {
+
+            duration = ( Math.abs(endPosition - startPosition) * 1.5);
+
+        }
+
+
+
+        requestAnimationFrame( animateLoop );
+
+
+
+        // Start music!
+
+        if( mainAudio ) {
+
+            mainAudio.play();
+
+        }
+
+
+
+        if( startCallback ) {
+
+            startCallback();
+
+        }
+
     };
 
+
+
     function browserMeetsRequirements() {
-      return window.requestAnimationFrame && window.Audio && window.addEventListener;
+
+        return window.requestAnimationFrame && window.Audio && window.addEventListener;
+
     }
 
+
+
     function resetPositions() {
-      startTime = null;
-      startPosition = null;
-      elevating = false;
+
+        startTime = null;
+
+        startPosition = null;
+
+        elevating = false;
+
     }
+
+
+
+    function updateEndPosition() {
+
+        if(targetElement){
+
+            endPosition = getVerticalOffset(targetElement);
+
+        }
+
+    }
+
+
 
     function animationFinished() {
 
-      resetPositions();
 
-      // Stop music!
-      if (mainAudio) {
-        mainAudio.pause();
-        mainAudio.currentTime = 0;
-      }
 
-      if (endAudio) {
-        endAudio.play();
-      }
+        resetPositions();
+
+
+
+        // Stop music!
+
+        if( mainAudio ) {
+
+            mainAudio.pause();
+
+            mainAudio.currentTime = 0;
+
+        }
+
+
+
+        if( endAudio ) {
+
+            endAudio.play();
+
+        }
+
+
+
+        if( endCallback ) {
+
+            endCallback();
+
+        }
+
     }
+
+
 
     function onWindowBlur() {
 
-      // If animating, go straight to the top. And play no more music.
-      if (elevating) {
 
-        cancelAnimationFrame(animation);
-        resetPositions();
 
-        if (mainAudio) {
-          mainAudio.pause();
-          mainAudio.currentTime = 0;
+        // If animating, go straight to the top. And play no more music.
+
+        if( elevating ) {
+
+
+
+            cancelAnimationFrame( animation );
+
+            resetPositions();
+
+
+
+            if( mainAudio ) {
+
+                mainAudio.pause();
+
+                mainAudio.currentTime = 0;
+
+            }
+
+
+
+            updateEndPosition();
+
+            window.scrollTo(0, endPosition);
+
         }
 
-        window.scrollTo(0, 0);
-      }
     }
 
-    function bindElevateToElement(element) {
-      if (element.addEventListener) {
-        element.addEventListener('click', that.elevate, false);
-      } else {
-        // Older browsers
-        element.attachEvent('onclick', function() {
-          document.documentElement.scrollTop = 0;
-          document.body.scrollTop = 0;
-          window.scroll(0, 0);
-        });
-      }
+
+
+    function bindElevateToElement( element ) {
+
+        if( element.addEventListener ) {
+
+            element.addEventListener('click', that.elevate, false);
+
+        } else {
+
+            // Older browsers
+
+            element.attachEvent('onclick', function() {
+
+                updateEndPosition();
+
+                document.documentElement.scrollTop = endPosition;
+
+                document.body.scrollTop = endPosition;
+
+                window.scroll(0, endPosition);
+
+            });
+
+        }
+
     }
 
-    function init(_options) {
-      // Bind to element click event, if need be.
-      body = document.body;
 
-      var defaults = {
-        duration: undefined,
-        mainAudio: false,
-        endAudio: false,
-        preloadAudio: true,
-        loopAudio: true,
-      };
 
-      _options = extendParameters(_options, defaults);
+    function init( _options ) {
 
-      if (_options.element) {
-        bindElevateToElement(_options.element);
-      }
+        // Bind to element click event, if need be.
 
-      // Take the stairs instead
-      if (!browserMeetsRequirements()) {
-        return;
-      }
+        body = document.body;
 
-      if (_options.duration) {
-        customDuration = true;
-        duration = _options.duration;
-      }
 
-      window.addEventListener('blur', onWindowBlur, false);
 
-      if (_options.mainAudio) {
-        mainAudio = new Audio(_options.mainAudio);
-        mainAudio.setAttribute('preload', _options.preloadAudio);
-        mainAudio.setAttribute('loop', _options.loopAudio);
-      }
+        var defaults = {
 
-      if (_options.endAudio) {
-        endAudio = new Audio(_options.endAudio);
-        endAudio.setAttribute('preload', 'true');
-      }
+            duration: undefined,
+
+            mainAudio: false,
+
+            endAudio: false,
+
+            preloadAudio: true,
+
+            loopAudio: true,
+
+            startCallback: null,
+
+            endCallback: null
+
+        };
+
+
+
+        _options = extendParameters(_options, defaults);
+
+
+
+        if( _options.element ) {
+
+            bindElevateToElement( _options.element );
+
+        }
+
+
+
+        // Take the stairs instead
+
+        if( !browserMeetsRequirements() ) {
+
+            return;
+
+        }
+
+
+
+        if( _options.duration ) {
+
+            customDuration = true;
+
+            duration = _options.duration;
+
+        }
+
+
+
+        if( _options.targetElement ) {
+
+            targetElement = _options.targetElement;
+
+        }
+
+
+
+        if( _options.verticalPadding ) {
+
+            verticalPadding = _options.verticalPadding;
+
+        }
+
+
+
+        window.addEventListener('blur', onWindowBlur, false);
+
+
+
+        if( _options.mainAudio ) {
+
+            mainAudio = new Audio( _options.mainAudio );
+
+            mainAudio.setAttribute( 'preload', _options.preloadAudio );
+
+            mainAudio.setAttribute( 'loop', _options.loopAudio );
+
+        }
+
+
+
+        if( _options.endAudio ) {
+
+            endAudio = new Audio( _options.endAudio );
+
+            endAudio.setAttribute( 'preload', 'true' );
+
+        }
+
+
+
+        if( _options.endCallback ) {
+
+            endCallback = _options.endCallback;
+
+        }
+
+
+
+        if( _options.startCallback ) {
+
+            startCallback = _options.startCallback;
+
+        }
+
     }
+
+
 
     init(options);
-  };
-})(window);
+
+};
+
+
+
+module.exports = Elevator;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,43 @@
 $(function () {
-	$('.tlt').textillate();
+	$('.tlt').show().textillate();
+
+
 });
+
+$( "h1" ).click(function() {
+  $( this ).fadeTo( "slow", 0.33 );
+});
+
+// Simple elevator usage.
+            var elementButton = document.querySelector('.elevator');
+            //var targetDiv = document.querySelector('.current');
+            var elevator = new Elevator({
+                element: elementButton,
+                //targetElement: targetDiv,
+                duration: 15000,
+                mainAudio: 'songs/motherearth.mp3', // Music from http://www.bensound.com/
+                endAudio:  'songs/sound.mp3',
+                startCallback: function() {
+                  //$('div.current').removeClass('current');
+                  //$(this).addClass
+
+
+
+                },
+                endCallback: function() {
+                  $(".tlt").fadeTo("slow", 0.33);
+
+                }
+            });
+
+function openNav() {
+    	document.getElementById("mySidenav").style.width = "250px";
+    	document.getElementById("mainContainer").style.marginLeft = "250px";
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+
+    function closeNav() {
+    	document.getElementById("mySidenav").style.width = "0";
+    	document.getElementById("mainContainer").style.marginLeft = "0";
+    	document.body.style.backgroundColor = "white";
+    }
